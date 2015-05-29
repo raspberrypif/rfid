110675df939f3c0c5c77d2e7e7648a6899711cb1
Subhajit Sahu <wolfram77@gmail.com>
2015-05-29 17:21:17 +0530

    index

--- index.js
+++ index.js
@@ -2,28 +2,22 @@
 
 
 // required modules
+var _ = require('lodash');
 var express = require('express');
-var sqlite3 = require('sqlite3').verbose();
 var config = require('./modules/config.js')('data/config.json');
-// var reader = require('./modules/reader.js');
-// var storage = require('./modules/storage.js');
-
-
-// config
-
+var reader = require('./modules/reader.js')(config.val.reader);
+var storage = require('./modules/storage.js')(config.val.storage);
 
 
 // initialize
 var app = express();
-var db = new sqlite3.Database('data/storage.db');
-/*
+var c = config.val.index;
 reader.on('card', function(cbits, card) {
-  storage.emit('add', [{'time': new Date(), 'point': 0, 'card': card}], function(err, val) {
-    reader.emit('beep');
-    console.log(err);
+  storage.emit('put', [{'time': new Date(), 'point': 0, 'card': card}], function(inv) {
+    console.log(JSON.stringify(inv));
+    if(inv.length > 0) { reader.emit('beep'); console.log('invalid!'); }
   });
 });
-*/
 
 
 // main page
@@ -32,22 +26,28 @@ app.get('/', function(req, res) {
 });
 
 
-// config interface
+// config get interface
 app.get('/api/config', function(req, res) {
+  res.json(config.val);
+});
+
+
+// config put interface
+app.post('/api/config', function(req, res) {
   res.json(config.val);
 });
 
 
 // reader.green interface
 app.all('/api/reader/green', function(req, res) {
-  // reader.emit('green');
+  reader.emit('green');
   res.send('0');
 });
 
 
 // reader.beep interface
 app.all('/api/reader/beep', function(req, res) {
-  // reader.emit('beep');
+  reader.emit('beep');
   res.send('0');
 });
 
@@ -59,9 +59,6 @@ app.use(express.static(__dirname+'/assets'));
 // start server
 var server = app.listen(80, function() {
   console.log('pi-snax');
-  db.get('SELECT COUNT(*) FROM temp', function(err, row) {
-    console.log(JSON.stringify(row));
-  });
 });
 
 
@@ -69,6 +66,6 @@ var server = app.listen(80, function() {
 // cleanup
 process.on('SIGINT', function() {
   server.close();
-  // reader.emit('close');
-  // storage.emit('close');
+  reader.close();
+  storage.close();
 });
