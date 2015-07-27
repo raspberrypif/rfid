ae0e6ed059680f825b720b933f6a7ec07bd0e778
Subhajit Sahu <wolfram77@gmail.com>
2015-07-27 15:05:20 +0530

    about.js replaced config.js

--- modules/about.js
+++ modules/about.js
@@ -1,134 +1,21 @@
 // @wolfram77
 // ABOUT - maintains info about device
-// () - point, points, get, set, clear, sync
+// () - id, team, all, load, save, close
+// .. - t = time, d = duration, g = gap, n = number, m = min, dev = device
 
 
 // required modules
 var EventEmitter = require('events').EventEmitter;
-var http = require('http');
-var z = require('./zed')();
 var _ = require('lodash');
 
 
 
 // initialize
-module.exports = function(c, tap) {
+module.exports = function(file) {
   var o = new EventEmitter();
 
   // init
-  var psync = [], esync = [];
-
-
-  // get request data for tap
-  var reqd  = function() {
-    var r = {}, now = _.now();
-    console.log('[group:reqd] t'+now);
-    for(var p in c.points)
-      r[p] = {'start': c.points[p].tsync+1, 'end': now};
-    return r;
-  };
-
-
-  // update sync time
-  var updatetsync = function(vs) {
-    console.log('[group:updatetsync]');
-    for(var p in vs)
-      if(vs[p].time && vs[p].time.length>0) c.points[p].tsync = _.last(vs[p].time);
-  };
-
-
-  // request options
-  var reqopt = function(p, len) {
-    console.log('[group:reqopt] .'+p+' ('+len+')');
-    return {
-      'method': 'GET',
-      'path': '/api/tap/get',
-      'host': c.points[p].host,
-      'port': c.points[p].port,
-      'headers': {
-        'Content-Type': 'application/json',
-        'Content-Length': len
-      }
-    };
-  };
-
-
-  // sync (one point)
-  var sync = function(p, fn) {
-    console.log('[group:sync] .'+p);
-    var sreq = JSON.stringify(reqd());
-    var options = reqopt(p, sreq.length);
-    var req = http.request(options, function(res) {
-      z.httpbody(res, function(sres) {
-        console.log('[group:sync] .'+p+' ok');
-        var resd = JSON.parse(sres);
-        updatetsync(resd);
-        tap.put(resd, function() {
-          if(fn) fn(true);
-        });
-      });
-    });
-    req.on('error', function(err) {
-      console.log('[group:sync] .'+p+' err');
-      if(fn) fn(false, err);
-    });
-    req.write(sreq);
-    req.end();
-  };
-
-
-  // get next point to sync
-  var nextsync = function(ps, all) {
-    console.log('[group:nextsync] .('+ps.length+')');
-    if(all) return ps[0];
-    while(ps.length > 0) {
-      var v = c.points[ps[0]];
-      if(v.tsync+v.gsync < _.now()) return ps[0];
-      ps.shift();
-    }
-  };
-
-
-  // sync loop
-  // ps = points, es = errors, all = do all
-  var syncloop = function(ps, es, all, fn) {
-    console.log('[group:syncloop]');
-    var p = nextsync(ps, all);
-    if(!p) {
-      if(fn) fn(es);
-      return;
-    }
-    sync(p, function(ok, err) {
-      ps.shift();
-      if(!ok) es.push([p, err]);
-      process.nextTick(function() {
-        syncloop(ps, es, all, fn);
-      });
-    });
-  };
-
-
-  // add point to sync list in intervals
-  var syncadd = function(p) {
-    console.log('[group:syncadd] .'+p);
-    setInterval(function() {
-      if(_.indexOf(psync, p) < 0) {
-        psync.push(p);
-        if(psync.length === 1) syncloop(psync, esync, false);
-      }
-    }, c.points[p].gsync);
-  };
-
-
-  // run sync in background
-  var syncrun = function() {
-    console.log('[group:syncrun]');
-    setInterval(function() {
-      esync.length = 0;
-    }, c.derr);
-    for(var p in c.points)
-      syncadd(p);
-  };
+  var c = {};
 
 
 
@@ -139,55 +26,57 @@ module.exports = function(c, tap) {
   };
 
 
-  // get or set peers
-  o.peers = function(peers) {
-    console.log('about.peers> %s', peers);
-    for(var i in peers) {
-      if(peers[i] === null) delete c.peers[i];
-      else _.merge(c.peers[i], peers[i]);
+  // get or set team (devices)
+  o.team = function(team) {
+    console.log('about.team> %j', team);
+    for(var i in team) {
+      if(team[i] === null) delete c.team[i];
+      else _.merge(c.team[i], team[i]);
     }
-    return c.peers;
+    return c.team;
   };
 
 
-  // state.js
-  var deltas = [{}];
+  // get or set all infor
+  o.all = function(all) {
+    console.log('about.all> %j', all);
+    if(all) _.merge(c, all);
+    return c;
+  };
 
-  // how do you take care of a given delta
-  o.delta = function(fn, args) {
 
+  // load
+  o.load = function(fn) {
+    console.log('about.load>');
+    fs.readFile(file, function(err, data) {
+      _.merge(c, JSON.parse(data));
+      if(fn) fn(c);
+    });
+  };
+
+
+  // save
+  o.save = function(fn) {
+    console.log('about.save>');
+    fs.writeFile(file, JSON.stringify(c), function(err) {
+      if(fn) fn();
+    });
+  };
+
+
+  // close module
+  o.close = function() {
+    console.log('about.close>');
+    fs.writeFileSync(file, JSON.stringify(c));
   };
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // sync data
-  o.sync = function(fn) {
-    console.log('[group.sync]');
-    var ps = _.keys(c.points), es = [];
-    syncloop(ps, es, true, fn);
-  };
-
-
-
-  // sync in background
-  syncrun();
-
+  // prepare
+  _.merge(c, JSON.parse(fs.readFileSync(file)));
+  setInterval(o.save, c.gsave);
 
   // ready!
-  console.log('[group] ready!');
+  console.log('about> ready!');
   return o;
 };
